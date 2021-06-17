a25560ff00cc847ed6e8dca1e428c807e28c2b05
Kou <ioiowax2020.jp@gmail.com>
2021-06-18 08:57:07 +0900

    install express-session,connect-flash,modify signup function with flash

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const app = express()
+const session = require('express-session')
+const flash = require('connect-flash')
+
+
 const port = 3000
 const db = require('./models') //引入資料庫
+
+
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true })) //解析req.body 來自使用者的請求
 
+app.use(session({ secret: 'Minesecret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port${port}`)
