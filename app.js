b8363db2958f11c3877c49a11a92fe3ad9be868f
Kou <ioiowax2020.jp@gmail.com>
2021-06-18 08:34:19 +0900

    add bcryptjs function,add userController and signup page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./models') //引入資料庫
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(express.urlencoded({ extended: true })) //解析req.body 來自使用者的請求
 
 
 app.listen(port, () => {
