4b832df0d6661aa00102ee468598948805e15bad
Kou <ioiowax2020.jp@gmail.com>
2021-06-19 20:25:01 +0900

    add admin:restaurant create form,and restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,7 +7,27 @@ const adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
-  }
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+  postRestaurants: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', `Please fill in the Restaurant's name!`)
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then((restaurants) => {
+        req.flash('success_messages', `${req.body.name} has successfully created!`)
+        res.redirect('/admin/restaurants')
+      })
+  },
 }
 
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,13 @@ module.exports = (app, passport) => {
 
   }
 
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
@@ -29,10 +35,5 @@ module.exports = (app, passport) => {
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
 
-  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', authenticated, restController.getRestaurants)
-
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
 }
