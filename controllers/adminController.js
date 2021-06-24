4b364572ef596cdb6b93b494afd91b446d9eb02d
Kou <ioiowax2020.jp@gmail.com>
2021-06-24 22:23:31 +0900

    add categories selector on admin create and edit page,and add selected option condition to category select

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -22,7 +22,14 @@ const adminController = {
       })
   },
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return res.render('admin/create', {
+        categories: categories
+      })
+    })
   },
   postRestaurants: (req, res) => {
 
@@ -41,7 +48,8 @@ const adminController = {
           address: req.body.address,
           opening_hours: req.body.opening_hours,
           description: req.body.description,
-          image: file ? img.data.link : null
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
@@ -55,7 +63,8 @@ const adminController = {
         address: req.body.address,
         opening_hours: req.body.opening_hours,
         description: req.body.description,
-        image: null
+        image: null,
+        CategoryId: req.body.categoryId
       })
         .then((restaurants) => {
           req.flash('success_messages', `${req.body.name} has successfully created!`)
@@ -73,10 +82,18 @@ const adminController = {
       })
   },
   editRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true })
-      .then(restaurant => {
-        return res.render('admin/create', { restaurant: restaurant })
-      })
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then((categories) => {
+      return Restaurant.findByPk(req.params.id)
+        .then(restaurant => {
+          return res.render('admin/create', {
+            categories: categories,
+            restaurant: restaurant.toJSON()
+          })
+        })
+    })
   },
   putRestaurant: (req, res) => {
 
@@ -96,7 +113,8 @@ const adminController = {
               address: req.body.address,
               opening_hours: req.body.opening_hours,
               description: req.body.description,
-              image: file ? img.data.link : restaurant.image
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
             }).then((restaurant) => {
               req.flash('success_messages', 'restaurant was successfully to update')
               res.redirect('/admin/restaurants')
@@ -113,7 +131,8 @@ const adminController = {
             tel: req.body.tel,
             address: req.body.address,
             opening_hours: req.body.opening_hours,
-            description: req.body.description
+            description: req.body.description,
+            CategoryId: req.body.categoryId
           })
             .then((restaurant) => {
               req.flash('success_messages', 'restaurant was successfully to update')
