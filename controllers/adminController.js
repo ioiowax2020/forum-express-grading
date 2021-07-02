7c6adee18ee4592936e0a9916fa9a553e36d4bc5
Kou <ioiowax2020.jp@gmail.com>
2021-07-02 23:12:15 +0900

    refactor PUT /api/admin/restaurants/:id

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -34,44 +34,6 @@ const adminController = {
       req.flash('success', data['message'])
       res.redirect('/admin/restaurants')
     })
-    // if (!req.body.name) {
-    //   req.flash('error_messages', `Please fill in the Restaurant's name!`)
-    //   return res.redirect('back')
-    // }
-
-    // const { file } = req
-    // if (file) {
-    //   imgur.setClientID(IMGUR_CLIENT_ID)
-    //   imgur.upload(file.path, (err, img) => {
-    //     return Restaurant.create({
-    //       name: req.body.name,
-    //       tel: req.body.tel,
-    //       address: req.body.address,
-    //       opening_hours: req.body.opening_hours,
-    //       description: req.body.description,
-    //       image: file ? img.data.link : null,
-    //       CategoryId: req.body.categoryId
-    //     }).then((restaurant) => {
-    //       req.flash('success_messages', 'restaurant was successfully created')
-    //       return res.redirect('/admin/restaurants')
-    //     })
-    //   })
-
-    // } else {
-    //   return Restaurant.create({
-    //     name: req.body.name,
-    //     tel: req.body.tel,
-    //     address: req.body.address,
-    //     opening_hours: req.body.opening_hours,
-    //     description: req.body.description,
-    //     image: null,
-    //     CategoryId: req.body.categoryId
-    //   })
-    //     .then((restaurants) => {
-    //       req.flash('success_messages', `${req.body.name} has successfully created!`)
-    //       res.redirect('/admin/restaurants')
-    //     })
-    // }
   },
   getRestaurant: (req, res) => {
 
@@ -94,50 +56,57 @@ const adminController = {
     })
   },
   putRestaurant: (req, res) => {
+    adminService.postRestaurants(req, res, (data) => {
+      if (data['status' === 'error']) {
 
-    if (!req.body.name) {
-      req.flash('error_messages', 'name dosent exist!')
-      return res.redirect('back')
-    }
-    const { file } = req
-    if (file) {
-      imgur.setClientID(IMGUR_CLIENT_ID)
-      imgur.upload(file.path, (err, img) => {
-        return Restaurant.findByPk(req.params.id)
-          .then((restaurant) => {
-            restaurant.update({
-              name: req.body.name,
-              tel: req.body.tel,
-              address: req.body.address,
-              opening_hours: req.body.opening_hours,
-              description: req.body.description,
-              image: file ? img.data.link : restaurant.image,
-              CategoryId: req.body.categoryId
-            }).then((restaurant) => {
-              req.flash('success_messages', 'restaurant was successfully to update')
-              res.redirect('/admin/restaurants')
-            })
-          })
-      })
+        req.flash('error_messages', data['message'])
+      }
+      req.flash('success', data['message'])
+      res.redirect('/admin/restaurants')
+    })
+    // if (!req.body.name) {
+    //   req.flash('error_messages', 'name dosent exist!')
+    //   return res.redirect('back')
+    // }
+    // const { file } = req
+    // if (file) {
+    //   imgur.setClientID(IMGUR_CLIENT_ID)
+    //   imgur.upload(file.path, (err, img) => {
+    //     return Restaurant.findByPk(req.params.id)
+    //       .then((restaurant) => {
+    //         restaurant.update({
+    //           name: req.body.name,
+    //           tel: req.body.tel,
+    //           address: req.body.address,
+    //           opening_hours: req.body.opening_hours,
+    //           description: req.body.description,
+    //           image: file ? img.data.link : restaurant.image,
+    //           CategoryId: req.body.categoryId
+    //         }).then((restaurant) => {
+    //           req.flash('success_messages', 'restaurant was successfully to update')
+    //           res.redirect('/admin/restaurants')
+    //         })
+    //       })
+    //   })
 
-    } else {
+    // } else {
 
-      return Restaurant.findByPk(req.params.id)
-        .then((restaurant) => {
-          restaurant.update({
-            name: req.body.name,
-            tel: req.body.tel,
-            address: req.body.address,
-            opening_hours: req.body.opening_hours,
-            description: req.body.description,
-            CategoryId: req.body.categoryId
-          })
-            .then((restaurant) => {
-              req.flash('success_messages', 'restaurant was successfully to update')
-              res.redirect('/admin/restaurants')
-            })
-        })
-    }
+    //   return Restaurant.findByPk(req.params.id)
+    //     .then((restaurant) => {
+    //       restaurant.update({
+    //         name: req.body.name,
+    //         tel: req.body.tel,
+    //         address: req.body.address,
+    //         opening_hours: req.body.opening_hours,
+    //         description: req.body.description,
+    //         CategoryId: req.body.categoryId
+    //       })
+    //         .then((restaurant) => {
+    //           req.flash('success_messages', 'restaurant was successfully to update')
+    //           res.redirect('/admin/restaurants')
+    //         })
+    //     })
+    // }
   },
   deleteRestaurant: (req, res) => {
     adminService.deleteRestaurant(req, res, (data) => {
